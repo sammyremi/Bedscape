e55230d08d3cf3513e1f48d235901d4b6a8f8f4d
oladayo Adekoya <adekoyadayo2010@gmail.com>
2023-11-21 11:51:01 +0100

    functioning react added

---
+++ app/javascript/controllers/react_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus";
+
+
+
+
+// Connects to data-controller="react"
+export default class extends Controller {
+  connect() {
+    console.log('react works');
+  }
+}
