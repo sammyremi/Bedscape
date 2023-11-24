24a8a80a68c72f802a6505675ad41f317b954bd4
sammyremi <adebayorsamuelas@gmail.com>
2023-11-24 08:23:06 +0100

    fix devise signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   resources :beds
+
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
